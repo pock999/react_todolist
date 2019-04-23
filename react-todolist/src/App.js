044a7a772c89ee00b51e6da8459b656750754a91
rickhou44 <rickhou44@gmail.com>
2019-04-24 00:11:15 +0800

    make todolist to pretty and add a new function that can clear all.

--- react-todolist/src/App.js
+++ react-todolist/src/App.js
@@ -26,13 +26,13 @@ function finishClass(fulfill){
 class List extends React.Component {
   render(){
     return (
-      <ul>
+      <ul className="ulist">
         {/* this.props 為其他地方使用List組件時候，所傳進來的參數 */}
       {this.props.list.map((item, idx) => (
        <li>
-         
+         {idx} :
          <span className={finishClass(item.fulfill)} onClick={() => this.props.onFinish(idx)}>
-          {idx} : {item.thing}
+           {item.thing}
          </span>
          -->          
          <span className="delete" onClick={() => this.props.onDelete(idx)}>
@@ -57,6 +57,11 @@ class Todo extends React.Component {
     this.deleteTodo = this.deleteTodo.bind(this);
     this.finishTodo = this.finishTodo.bind(this);
     this.keyPress = this.keyPress.bind(this);
+    this.clearAll = this.clearAll.bind(this);
+  }
+  clearAll(){
+    const updatedList = [];
+    this.setState({ list: updatedList , input: ""});
   }
   keyPress(event){
     console.log(event.keyCode);
@@ -78,9 +83,14 @@ class Todo extends React.Component {
   addTodo() {
     // 語法:updatedList = [...原先的ary,新增的值]
     // const updatedList = [...this.state.list, this.state.input];
-    const updatedList = [...this.state.list, new TodoThing(this.state.input,false)];
-    // 更新state的方式 ==> this.setState({屬性一:更新的屬性值,屬性二:更新的屬性值});
-    this.setState({ list: updatedList, input: "" });
+    if(this.state.input==''){
+      alert("請輸入待辦事項");
+    }else{
+      const updatedList = [...this.state.list, new TodoThing(this.state.input,false)];
+      // 更新state的方式 ==> this.setState({屬性一:更新的屬性值,屬性二:更新的屬性值});
+      this.setState({ list: updatedList, input: "" });
+    }
+    
   }
   deleteTodo(idx) {
     // 這裡的意思是只留下index不等於傳進來要刪除的idx的項目，也就是過濾掉idx的項目
@@ -96,11 +106,12 @@ class Todo extends React.Component {
     return (
       <div>
         {/* 使input內的值可以被更改，因為這裡的value原先已經固定給state的屬性，在這裡使期變成雙向繫結*/}
-        <input value={input} onChange={this.handleInput} onKeyDown={this.keyPress}/>
+        <input type="text" value={input} onChange={this.handleInput} onKeyDown={this.keyPress} placeholder="add todo" className="input-thing"/>
 
-        <button onClick={this.addTodo}>add</button>
+        <button onClick={this.addTodo} className="btnadd">add</button>
         {/* 在List 內可以使用this.props.list呼叫到list，使用this.props.onDelet可以呼叫到deleteTodo */}
         <List list={list} onDelete={this.deleteTodo} onFinish={this.finishTodo}/>
+        <button onClick={this.clearAll} className="clear-all">clear</button>
       </div>
     );
   }
